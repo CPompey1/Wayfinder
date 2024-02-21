6b535b6b4ce19a6426b695228ab1aef02339bbfa
demons372617 <2054155643@qq.com>
2024-02-21 17:14:12 -0500

    create func tra_localization to be able to called

--- tra_localization.py
+++ tra_localization.py
@@ -1,24 +1,34 @@
 from scipy.optimize import minimize
 import numpy as np
+import math
 
-# Example known points (x, y, z)
-points = np.array([[0, 0, 0], [10, 0, 0], [0, 10, 0]])
+def tra_localization(signal_a, signal_b, signal_c, name_a, name_b, name_c):
 
-# Example distances from the unknown point to each of the known points
-distances = np.array([5, 7.07, 7.07])
+    point_a = emitter_location_dic[name_a]
+    point_b = emitter_location_dic[name_b]
+    point_c = emitter_location_dic[name_c]
+    # Example known points (x, y, z)
+    points = np.array([point_a, point_b, point_c])
 
-# Objective function: sum of squared differences between actual and calculated distances
-def objective_function(unknown, points, distances):
-    return sum((np.linalg.norm(unknown - points[i]) - distances[i])**2 for i in range(len(points)))
+    dis_a = 9.2143 * math.log(signal_a) + 47.283
+    dis_b = 9.2143 * math.log(signal_b) + 47.283
+    dis_c = 9.2143 * math.log(signal_c) + 47.283
 
-# Initial guess for the unknown point
-initial_guess = np.mean(points, axis=0)
+    # Example distances from the unknown point to each of the known points
+    distances = np.array([dis_a, dis_b, dis_c])
 
-# Minimize the objective function
-result = minimize(objective_function, initial_guess, args=(points, distances))
+    # Objective function: sum of squared differences between actual and calculated distances
+    def objective_function(unknown, points, distances):
+        return sum((np.linalg.norm(unknown - points[i]) - distances[i])**2 for i in range(len(points)))
 
-# Extract the estimated location of the unknown point
-estimated_location = result.x
+    # Initial guess for the unknown point
+    initial_guess = np.mean(points, axis=0)
 
-print("Estimated location:", estimated_location)
+    # Minimize the objective function
+    result = minimize(objective_function, initial_guess, args=(points, distances))
+
+    # Extract the estimated location of the unknown point
+    estimated_location = result.x
+
+    print("Estimated location:", estimated_location)
 
