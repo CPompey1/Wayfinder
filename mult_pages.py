9e456ae858cf805c1750f746e6c95056e94d711a
Scilla Benussi <78547322+scillabenussi@users.noreply.github.com>
2024-04-01 06:35:47 +0200

    new updates
    Developer mode passcode available.
    Multiple pages instead of multiple windows almost completed: mult_pages file

---
+++ mult_pages.py
@@ -0,0 +1,218 @@
+from tkinter import *
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+from tkinter import Canvas
+import ttkbootstrap as ttkint
+from PIL import ImageTk, Image, ImageOps
+import json
+
+FONT = "Calibri 26 bold"
+FONT_SERVICES = "Calibri 15"
+
+def flatten(list_of_list):
+    if isinstance(list_of_list, list):
+        fin_list = []
+        for el in list_of_list:
+            fin_list.extend(flatten(el))
+        return fin_list
+    else:
+        return [list_of_list]
+	
+class tkinterApp(tk.Tk):
+	def __init__(self, *args, **kwargs): 
+		tk.Tk.__init__(self, *args, **kwargs)
+		#self.geometry("%dx%d" % (self.winfo_screenwidth(), self.winfo_screenheight()))
+		self.geometry("768x1024")
+		# Container
+		container = tk.Frame(self) 
+		self.title("Wayfinder")
+		container.pack(side = "top", fill = "both", expand = True) 
+		container.grid_rowconfigure(0, weight = 1)
+		container.grid_columnconfigure(0, weight = 1)
+		container.grid_rowconfigure(1, weight = 1)
+		container.grid_columnconfigure(1, weight = 1)
+		container.grid_rowconfigure(2, weight = 1)
+		container.grid_columnconfigure(2, weight = 1)
+		container.grid_rowconfigure(3, weight = 1)
+		container.grid_columnconfigure(3, weight = 1)
+
+
+		# initializing frames to an empty array
+		self.frames = {} 
+		self.json_file_dict: dict = json.load(open("services.json"))
+		self.nodes: dict = json.load(open("node.json"))
+		self.service_array = list(self.json_file_dict["service_group"][0].keys())
+		self.dest_list = list(self.nodes["destinations"])
+        #print(self.dest_list)
+		all_services = []
+		for i in range(len(self.service_array)):
+			floor_array = self.json_file_dict["service_group"][0][self.service_array[i]][0]
+			all_services.append(flatten(list(floor_array.values())))
+		all_services = flatten(all_services)
+        # Filter not None
+		self.all_valid_serv = list(filter(None,all_services))
+        # Order alphabetically
+		self.all_valid_serv = sorted(self.all_valid_serv)
+        # Default stairs selection
+		self.stairs = BooleanVar(value=True)
+		self.selected = False
+		self.sel_service = "All services"
+		self.sel_room = "Second Floor Elevator"
+		# iterating through a tuple consisting
+		# of the different page layouts
+		for F in (StartPage, ServicesSearch, NavigationPage, DeveloperMode):
+
+			frame = F(container, self)
+
+			# initializing frame of that object from
+			# startpage, page1, page2 respectively with 
+			# for loop
+			self.frames[F] = frame 
+
+			frame.grid(row = 0, column = 0, columnspan=4, sticky ="nsew")
+
+		self.show_frame(StartPage)
+		
+
+
+	# to display the current frame passed as
+	# parameter
+	def show_frame(self, cont):
+		frame = self.frames[cont]
+		frame.tkraise()
+
+# first window frame startpage
+
+class StartPage(tk.Frame):
+	def __init__(self, parent, controller): 
+		
+		tk.Frame.__init__(self, parent)
+		title_label = Label(self, text= "Welcome to Lockwood Wayfinder!", font=FONT)
+		title_label.pack(pady=10)
+		img = ImageTk.PhotoImage(Image.open("lockwood_main.jpg"))
+		panel = Label(self, image=img)
+		panel.image = img
+		panel.pack(pady=10)
+		start_frame = Frame(self, bg="white")
+		start_button = ttk.Button(master=start_frame, text= "Start Navigating", command = lambda : controller.show_frame(ServicesSearch))
+		start_button.pack(pady=10)
+		dev_mode_button = ttk.Button(master=start_frame, text= "Developer Mode", command = lambda : controller.show_frame(DeveloperMode))
+		dev_mode_button.pack(pady=10)
+		start_frame.pack()
+
+class ServicesSearch(tk.Frame):
+	
+	def __init__(self, parent, controller: tkinterApp):
+		self.controller = controller
+		tk.Frame.__init__(self, parent)
+		self.services_lb = tk.Listbox(self, height = len(controller.service_array)+1, width = 10, font=FONT, name='service_list')
+		# Include an "all services" category with all reachable services
+		self.services_lb.insert(tk.END, "All services")
+		for item in controller.service_array:
+			self.services_lb.insert(tk.END, item)
+		self.services_lb.grid(column=0, row=0, sticky='nw', padx=0, pady=2)
+		self.services_lb.bind('<<ListboxSelect>>', self.onselect)
+		self.services_lb.select_set(0)
+		self.room_lb = Listbox(self, font=FONT_SERVICES, width = 60, name='room_list', selectmode="SINGLE")
+		for room in controller.all_valid_serv:
+			self.room_lb.insert(tk.END, room)
+		self.room_lb.grid(column=1, columnspan=4, row=0, sticky='nwse', padx=0, pady=2)
+		self.room_lb.bind('<Double-1>', self.service_confirmation)
+		self.selected = False
+		back_butt = ttk.Button(self, text= "Back", command = lambda : controller.show_frame(StartPage))
+		back_butt.grid(column=0, row=2,sticky="nw", padx=10, pady = 6)
+		frame = tk.Frame(self)
+		frame.grid(row = 2, column = 1, sticky="nwes",  padx=10, pady = 6)
+		s_or_el = ttk.Label(frame, text ="Stairs/Elevator Preference: ", font = FONT_SERVICES).pack()
+		R1 = Radiobutton(frame, text="Stairs", variable=self.controller.stairs, value=True).pack()
+		R2 = Radiobutton(frame, text="Elevator", variable=self.controller.stairs, value=False).pack()
+		self.controller.sel_service = "All services"	
+		self.controller.sel_room = "Second Floor Elevator" 
+
+	def onselect(self, evt: Event):
+		if not evt.widget.curselection():
+			return
+		# Note here that Tkinter passes an event object to onselect()
+		w = evt.widget.curselection()[0] - 1 # Number from 0 to n number of services categories to exclude "ALL" category
+		self.controller.sel_service = str(self.services_lb.get(self.services_lb.curselection()))
+		floor_array = {}
+		print(self.controller.sel_service)
+		# Side menu depending on selected service
+		floor_array = self.controller.json_file_dict["service_group"][0][self.controller.service_array[w]][0]
+		list_services = flatten(list(floor_array.values()))
+		# Listbox for rooms
+		self.room_lb = Listbox(self, font=FONT_SERVICES, name='room_list', selectmode="SINGLE")
+		if w == -1:
+			for room in self.controller.all_valid_serv:
+				self.room_lb.insert(tk.END, room)
+		else:
+			floor_array = self.controller.json_file_dict["service_group"][0][self.controller.service_array[w]][0]
+			list_services = flatten(list(floor_array.values()))
+			for room in list_services:
+				self.room_lb.insert(tk.END, room)
+		self.room_lb.grid(column=1, columnspan=4, row=0, sticky='nwse', padx=0, pady=2)
+		self.room_lb.bind('<Double-1>', self.service_confirmation)
+		self.grid_columnconfigure(1, weight=5)
+		#page1.state('zoomed')
+		self.bind("<Escape>", lambda e: self.quit())
+		# Listbox for services
+
+	def service_confirmation(self, args: Event):
+		if not args.widget.curselection():
+			return
+		if not self.controller.selected:
+			self.controller.selected = True
+			idx = args.widget.curselection()[0]
+			if self.controller.sel_service != "All services":
+				floor_services_flat= flatten(list(self.controller.json_file_dict["service_group"][0][self.controller.sel_service][0].values()))
+			    # Getting rid of None values from json fileù
+				serv_not_none = list(filter(None,floor_services_flat))
+				self.controller.sel_room  = serv_not_none[idx]
+			else:
+			    # If selected from the "All services" list
+				self.controller.sel_room = self.controller.all_valid_serv[idx]
+			print(self.controller.sel_service)
+			mess= "Selected service: \""
+			mess = mess + str(self.controller.sel_room)
+			mess = mess + "\". \nWould you like to proceed?"
+			selection = messagebox.askyesno(title="Service confirmation", message=mess, parent=self)
+			if selection:
+				#self.start_navigation(sel_service)
+				self.controller.show_frame(NavigationPage)
+			else:
+				self.controller.selected = False
+
+
+
+# third window frame page2
+class NavigationPage(tk.Frame): 
+	def __init__(self, parent, controller):
+		self.controller = controller
+		tk.Frame.__init__(self, parent)
+		mess = "Goal: "+ self.controller.sel_room
+		nav_label = Label(self, text= "Wayfinder Navigation", font=FONT).pack()
+		service_label = Label(self, text= mess, font=FONT_SERVICES).pack()
+	
+
+		# button to show frame 2 with text
+		# layout2
+		button1 = ttk.Button(self, text ="Page 1",
+							command = self.reset_service)
+	
+		# putting the button in its place by 
+		# using grid
+		button1.pack()
+
+	def reset_service(self):
+		self.controller.selected = False
+		self.controller.show_frame(ServicesSearch)
+
+class DeveloperMode(tk.Frame):
+	def __init__(self, parent, controller):
+		tk.Frame.__init__(self, parent)
+
+
+# Driver Code
+app = tkinterApp()
+app.mainloop()
--- wayfinder.py
+++ wayfinder.py
@@ -22,11 +22,14 @@ def flatten(list_of_list):
 
 class Wayfinder_UI:
     global service_array
-    def __init__(self, file):
+    def __init__(self, serv_file, nodes_file):
     # MAIN PAGE FOR WELCOME
     # Change this based on display dimensions
-        self.json_file_dict: dict = file
+        self.json_file_dict: dict = serv_file
+        self.nodes: dict = nodes_file
         self.service_array = list(self.json_file_dict["service_group"][0].keys())
+        self.dest_list = list(self.nodes["destinations"])
+        #print(self.dest_list)
         all_services = []
         for i in range(len(self.service_array)):
             floor_array = self.json_file_dict["service_group"][0][self.service_array[i]][0]
@@ -160,27 +163,43 @@ class Wayfinder_UI:
         start = [0,0]
         goal = [40, 550]
 
-        draw_path(nav_page, start, goal)
+        self.draw_path(nav_page, start, goal)
 
         nav_page.mainloop()
-
+    
     # DEVELOPER MODE TO CHECK ON EMITTERS FUNCTIONALITIES
     def developer_mode(self):
-        dev_page = Tk()
-        dev_page.configure(background="white")
-        dev_page.geometry("%dx%d" % (dev_page.winfo_screenwidth(), dev_page.winfo_screenheight()))
-        dev_page.title('Insert Password for Developer Mode')
+        #self.input_text = StringVar()
+        #self.input_text.set("")
+        #dev_page = tk.Tk()
+        #dev_page.configure(background="white")
+        #dev_page.geometry("%dx%d" % (dev_page.winfo_screenwidth(), dev_page.winfo_screenheight()))
+        #dev_page.title('Insert Password for Developer Mode')
         #dev_page.state('zoomed')
         self.master.destroy()
-        self.master = dev_page
-        pass_frame = Frame(master=dev_page, bg="white")
-        pass_frame.pack(side = TOP)
-        input_text = StringVar()
-
-
-
-
+        passcode_entry = PasscodeEntry(self)
         self.master.mainloop()
+        """ self.passcode_entry = tk.Entry(dev_page, textvariable=self.input_text, show='*', font=("Arial", 14))
+        self.passcode_entry.grid(row=1, column=0, rowspan=2, columnspan=3, pady=10)
+        #input_field = Entry(pass_frame, font = ('arial', 18, 'bold'), textvariable = self.input_text, justify = CENTER)
+        #input_field.pack()
+        #btns_frame = Frame(dev_page, width = dev_page.winfo_screenwidth(), bg = "grey")
+
+        clear = Button(dev_page, text = "Clear", width=15, height = 5, command = lambda: self.btn_clear(),font='Calibri 12').grid(row = 3, column = 0)
+        enter = Button(dev_page, text = "Enter", width=15, height = 5, command = lambda: self.btn_enter(),font='Calibri 12').grid(row = 3, column = 2)
+        b1 = Button(dev_page, text='1', width=15, height = 5, command= lambda:   lambda: self.btn_click('1'),font='Calibri 12').grid(row = 0, column = 0)
+        b2 = Button(dev_page, text='2', width=15, height = 5, command=  lambda: self.btn_click('2'),font='Calibri 12').grid(row = 0, column = 1)
+        b3 = Button(dev_page, text='3', width=15, height = 5, command=  lambda: self.btn_click('3'), font='Calibri 12').grid(row = 0, column = 2)
+        b4 = Button(dev_page, text='4', width=15, height = 5, command=  lambda: self.btn_click('4'), font='Calibri 12').grid(row = 1, column = 0)
+        b5 = Button(dev_page, text='5', width=15, height = 5, command=  lambda: self.btn_click('5'), font='Calibri 12').grid(row = 1, column = 1)
+        b6 = Button(dev_page, text='6', width=15, height = 5,  command=  lambda: self.btn_click('6'), font='Calibri 12').grid(row = 1, column = 2)
+        b7 = Button(dev_page, text='7', width=15, height = 5, command=  lambda: self.btn_click('7'), font='Calibri 12').grid(row = 2, column = 0)
+        b8 = Button(dev_page, text='8', width=15, height = 5,  command=  lambda: self.btn_click('8'),font='Calibri 12').grid(row = 2, column = 1)
+        b9 = Button(dev_page, text='9', width=15, height = 5, command=  lambda: self.btn_click('9'),font='Calibri 12').grid(row = 2, column = 2)
+        b0 = Button(dev_page, text='0', width=15, height = 5, command=  lambda: self.btn_click('0'),font='Calibri 12').grid(row = 3, column = 1)
+        #dev_page.pack()
+
+        self.master.mainloop() """
 
     # Pop-up message to select stairs over elevator
     def stairs_or_el(self):
@@ -211,32 +230,114 @@ class Wayfinder_UI:
                 self.start_navigation(sel_service)
             else:
                 self.selected = False
+    def btn_clear(self):
+        self.input_text.set("")
 
-# Function that draws lines for directions to follow
-def draw_path(page: Tk, start, goal):
-    #screen = Canvas(page, width= 600, height=550, background="black")
-    # screen.pack(anchor='nw', fill='both', expand=1)
-    w = page.winfo_screenwidth()
-    h = page.winfo_screenheight()
-    screen = Canvas(master=page, width=w, height=h)
-    screen.pack(anchor="center")
-    img_floor = Image.open("flr4.jpg")
-    img_floor = ImageOps.exif_transpose(img_floor)
-    width, height = int(img_floor.width / 2), int(img_floor.height / 2) 
-    img = img_floor.resize((width,height), Image.Resampling.LANCZOS)#.rotate(-90)
-    screen.configure(background="white", width=img.width, height=height)
-    img_tk = ImageTk.PhotoImage(img)
-    screen.image = img_tk
-    #img_floor = img_floor.resize((500,500), Image.ANTIALIAS)
-    # Play with position
-    screen.create_image(0,0, image= img_tk, anchor= "nw")
-    #screen.create_line(start[0],start[1],goal[0], goal[1], fill="red", width = 3)
-    page.mainloop()
+    def btn_click(self, item):
+        self.input_text.set(self.input_text.get() + item)
+
+    def btn_enter(self):
+        # Write code for passcode check
+        print(self.passcode_entry.get())
+
+
+
+    # Function that draws lines for directions to follow
+    def draw_path(page: Tk, start, goal):
+        #screen = Canvas(page, width= 600, height=550, background="black")
+        # screen.pack(anchor='nw', fill='both', expand=1)
+        # https://www.youtube.com/watch?v=5V_cPy2dtTc
+
+        w = page.winfo_screenwidth()
+        h = page.winfo_screenheight()
+        screen = Canvas(master=page, width=w, height=h)
+        screen.pack(anchor="center")
+        img_floor = Image.open("flr4.jpg")
+        img_floor = ImageOps.exif_transpose(img_floor)
+        width, height = int(img_floor.width / 2), int(img_floor.height / 2) 
+        img = img_floor.resize((width,height), Image.Resampling.LANCZOS)#.rotate(-90)
+        screen.configure(background="white", width=img.width, height=height)
+        img_tk = ImageTk.PhotoImage(img)
+        screen.image = img_tk
+        #img_floor = img_floor.resize((500,500), Image.ANTIALIAS)
+        # Play with position
+        screen.create_image(0,0, image= img_tk, anchor= "nw")
+        #screen.create_line(start[0],start[1],goal[0], goal[1], fill="red", width = 3)
+        page.mainloop()
+
+
+class PasscodeEntry:
+    def __init__(self, ui: Wayfinder_UI):
+        self.root = Tk()
+        self.root.configure(background="white")
+        self.root.geometry("%dx%d" % (self.root.winfo_screenwidth(), self.root.winfo_screenheight()))
+        self.root.title('Insert Password for Developer Mode')
+        passcode_frame = Frame(master=self.root, bg="white")
+        self.master = passcode_frame
+        ui.master = self.root
+        #self.master.title("Passcode Entry")
+        self.passcode = tk.StringVar()
+        self.passcode.set("")
+        self.enter = False      # True when Enter is pressed
+        self.right_wrong_label = tk.Label(self.root, text = "")
+        
+        self.entry_label = tk.Label(self.master, text="Enter Passcode:")
+        self.entry_label.grid(row=0, column=0, columnspan=3)
+
+        self.passcode_entry = tk.Entry(self.master, textvariable=self.passcode, show='*', font=("Arial", 14))
+        self.passcode_entry.grid(row=1, column=0, columnspan=3, pady=10)
+
+        self.keypad_buttons = [
+            '1', '2', '3',
+            '4', '5', '6',
+            '7', '8', '9',
+        ]
+
+        row_num = 2
+        col_num = 0
+        for key in self.keypad_buttons:
+            button = tk.Button(self.master, text=key, width=5, height=2, command=lambda key=key: self.update_passcode(key))
+            button.grid(row=row_num, column=col_num, padx=5, pady=5)
+            col_num += 1
+            if col_num > 2:
+                col_num = 0
+                row_num += 1
+        
+        
+        self.clear_button = tk.Button(self.master, text="Clear", width=5, height=2, command=self.clear_passcode)
+        self.clear_button.grid(row=row_num, column=0, padx=5, pady=5)
+        zero_button = tk.Button(self.master, text='0', width=5, height=2, command=lambda key=key: self.update_passcode('0'))
+        zero_button.grid(row=row_num, column=1, padx=5, pady=5)
+        self.enter_button = tk.Button(self.master, text="Enter", width=5, height=2, command=self.enter_passcode)
+        self.enter_button.grid(row=row_num, column=2, padx=5, pady=5)
+        self.master.pack()
+
+    def update_passcode(self, key):
+        if not self.enter:
+            self.passcode.set(self.passcode.get() + key)
+
+    def clear_passcode(self):
+        self.passcode.set("")
+        self.enter = False
+
+    def enter_passcode(self):
+        print("Passcode entered:", self.passcode.get())
+        #self.right_label = tk.Label(self.root, text = "Right passcode!").pack()
+        if self.passcode.get() == str(3054):
+            self.right_wrong_label.configure(text="Right passcode!")
+            self.enter = True
+        else:
+            self.right_wrong_label.configure(text="Wrong passcode!")
+        self.right_wrong_label.pack()
+        self.passcode.set("")
+
+
 
 if __name__ == '__main__': 
     # Read json file for services and rooms 
-    filename = open("services.json")
-    services_from_jason = json.load(filename)
-    #print(services_from_jason)
+    services_filename = open("services.json")
+    nodes_filename = open("node.json")
+    services_json = json.load(services_filename)
+    nodes_json = json.load(nodes_filename)
     # Call Wayfinder UI
-    wayfinder = Wayfinder_UI(services_from_jason)
+    wayfinder = Wayfinder_UI(services_json, nodes_json)
